a8b13de3494a30097f8e4463e1cdb67884ef29c4
Kevin Blount <kvbchamp@gmail.com>
2020-08-04 16:06:50 -0400

    renamed view function to index_view

--- hello_app/views.py
+++ hello_app/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 from . import views
 
 
-def index(request):
+def index_view(request):
     my_greeting = {"greeting": "Hello, World!"}
     return render(request, "index.html", my_greeting)
--- hello_django/urls.py
+++ hello_django/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from hello_app.views import index
+from hello_app.views import index_view
 
 
 urlpatterns = [
